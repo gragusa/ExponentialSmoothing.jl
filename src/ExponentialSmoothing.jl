c2448f0af0a47a2a0cf23d3505f5b67e5e2689a0
Giuseppe Ragusa <giuseppe.ragusa@gmail.com>
2018-11-07 09:54:24 +0100

    Move pkg's using here

--- src/ExponentialSmoothing.jl
+++ src/ExponentialSmoothing.jl
@@ -1,4 +1,13 @@
 module ExponentialSmoothing
 
+using Optim
+using Statistics
+using StatsFuns
+using TimeSeries
+using RecipesBase
+using Colors
+using ColorSchemes
+
 include("main.jl")
+
 end # module
